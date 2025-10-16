3ccc8f19f39dff2368744b25c274ebbb7c30e4a1
karl <karldamb27@gmail.com>
2025-10-16 12:01:51 +0200

    Feat: added Basic game logic, missing DB

---
+++ Teslow-srv.service/Game/Interface/IGameService.cs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teslow_srv.Domain.Dto.Game;
+
+namespace Teslow_srv.Service.Game.Interface
+{
+    public class IGameService
+    {
+        Task<IEnumerable<ReadGameDto>> GetAllAsync();
+        Task<ReadGameDto?> GetByIdAsync(Guid id);
+        Task<ReadGameDto> CreateAsync(CreateGameDto dto);
+        Task<ReadGameDto?> UpdateAsync(Guid id, UpdateGameDto dto);
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
