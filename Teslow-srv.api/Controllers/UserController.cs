eeee170cdd3d6fb9ba8d583ef4e0ec30def6ebf5
edgo <edgar.andre@ynov.com>
2025-10-16 10:57:20 +0200

    init commit

---
+++ Teslow-srv.api/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Teslow_srv.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [Route("user")]
+        [HttpPost]
+        public  CreateUser()
+
+        [Route("user")]
+        [HttpGet]
+
+
+        [Route("user")]
+        [HttpGet]
+    }
+}
---
+++ Teslow-srv.domain/Dto/User/ResponseCreateUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Teslow_srv.domain.Dto.User
+{
+    private class ResponseCreateUserDto
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
